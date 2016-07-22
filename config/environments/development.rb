e10618f17c15815cf2999b88f69f9514ea63da75
jessonfoo <jessonfoo@gmail.com>
2016-07-21 19:50:07 -0700

    dev env change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,17 +7,17 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
-  config.action_mailer.default_url_options = { host: 'https://pokeymongoshow.herokuapp.com/', port: 80 }
+  # config.eager_load = false
+  config.action_mailer.default_url_options = { host: 'pokeymongoshow.herokuapp.com', port: 80 }
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  # config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  # config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  # config.assets.debug = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
